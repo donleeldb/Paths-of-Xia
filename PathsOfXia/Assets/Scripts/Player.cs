3c30795a78e255630c7ff1c21393f267abb050c3
Don <donlee940125@gmail.com>
2019-01-01 17:36:27 -0500

    added unity project

---
+++ PathsOfXia/Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private float QingGong = 1f;
+    [SerializeField] private float YingGong = 1f;
+    [SerializeField] private float RuanGong = 1f;
+    [SerializeField] private float YanGong = 1f;
+    [SerializeField] private float ErGong = 1f;
+    [SerializeField] private float NeiGongYang = 1f;
+    [SerializeField] private float NeiGongYin = 1f;
+
+    private string playerState;
+    private string playerStatus;
+
+
+    // Use this for initialization
+    void Start () {
+        //playerState = "Home";
+        //playerStateText.text = "started";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //playerQingGongText.text = "轻功值：" + QingGong + "\n";
+        //playerQingGongText.text = "硬功值：" + YingGong + "\n";
+        //playerQingGongText.text = "软功值：" + RuanGong + "\n";
+        //playerQingGongText.text = "眼功值：" + YanGong + "\n";
+        //playerQingGongText.text = "耳功值：" + ErGong + "\n";
+        //playerQingGongText.text = "阳内功值：" + NeiGongYang + "\n";
+        //playerQingGongText.text = "阴内功值：" + NeiGongYin + "\n";
+    }
+
+    // Trainings 
+    public void TrainQingGong() {
+        playerState = "TrainingQingGong";
+    }
+    public void TrainYingGong() {
+        playerState = "TrainingYingGong";
+    }
+    public void TrainRuanGong() {
+        playerState = "TrainingRuanGong";
+    }
+    public void TrainYanGong() {
+        playerState = "TrainingYanGong";
+    }
+    public void TrainErGong() {
+        playerState = "TrainingErGong";
+    }
+
+    // Get Player's state
+    public string GetPlayerState() {
+        return playerState;
+    }
+
+    // AbilitiesGetters
+    public float GetQingGong() {
+        return QingGong;
+    }
+    public float GetYingGong()
+    {
+        return YingGong;
+    }
+    public float GetRuanGong()
+    {
+        return RuanGong;
+    }
+    public float GetYanGong()
+    {
+        return YanGong;
+    }
+    public float GetErGong()
+    {
+        return ErGong;
+    }
+    public float GetYangNeiGong()
+    {
+        return NeiGongYang;
+    }
+    public float GetYinNeiGong()
+    {
+        return NeiGongYin;
+    }
+}
---
+++ PathsOfXia/Assets/Scripts/UI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UI : MonoBehaviour {
+
+    [SerializeField] private GameObject player;
+    public Text AbilitiesText;
+    public Text PlayerStateText;
+    // Use this for initialization
+    void Start () {
+        GameObject.Find("TrainQingGong").GetComponentInChildren<Text>().text = "修炼轻功";
+        GameObject.Find("TrainYingGong").GetComponentInChildren<Text>().text = "修炼硬功";
+        GameObject.Find("TrainRuanGong").GetComponentInChildren<Text>().text = "修炼软功";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerStateText.text = "角色正在" + player.gameObject.GetComponent<Player>().GetPlayerState();
+
+        AbilitiesText.text = "角色能力\n";
+        AbilitiesText.text += "轻功值：" + player.gameObject.GetComponent<Player>().GetQingGong() + "\n";
+        AbilitiesText.text += "硬功值：" + player.gameObject.GetComponent<Player>().GetYingGong() + "\n";
+        AbilitiesText.text += "软功值：" + player.gameObject.GetComponent<Player>().GetRuanGong() + "\n";
+        AbilitiesText.text += "眼功值：" + player.gameObject.GetComponent<Player>().GetYanGong() + "\n";
+        AbilitiesText.text += "耳功值：" + player.gameObject.GetComponent<Player>().GetErGong() + "\n";
+        AbilitiesText.text += "内功值（阳）：" + player.gameObject.GetComponent<Player>().GetYangNeiGong() + "\n";
+        AbilitiesText.text += "内功值（阴）：" + player.gameObject.GetComponent<Player>().GetYinNeiGong() + "\n";
+
+    }
+
+    public void TrainQingGong() {
+        player.gameObject.GetComponent<Player>().TrainQingGong();
+    }
+
+    public void TrainYingGong() {
+        player.gameObject.GetComponent<Player>().TrainYingGong();
+    }
+
+    public void TrainRuanGong() {
+        player.gameObject.GetComponent<Player>().TrainRuanGong();
+    }
+}
